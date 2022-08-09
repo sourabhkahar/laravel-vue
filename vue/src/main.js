da44d018adc06fe8afc5fb18a3e7e47500e8639a
sourabhkahar <sourabhkahar8140@gmail.com>
2022-08-09 07:51:27 +0530

    first commit

---
+++ vue/src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+// import './style.css'
+import App from './App.vue'
+import store from './store/index.js'
+import router from './router/index.js'
+import './index.css'
+createApp(App)
+.use(store)
+.use(router)
+.mount('#app')
+
+
---
+++ vue/src/router/index.js
@@ -0,0 +1,39 @@
+import { createRouter,createWebHistory  } from "vue-router";
+import Dashboard from "../views/Dashboard.vue";
+import Registration from "../views/Registration.vue";
+import Login from "../views/Login.vue";
+import Surveys from "../views/Surveys.vue";
+import DefaultLayout from '../components/DefaultLayout.vue'
+import store from "../store";
+const routes= [
+    {        
+        path:'/',
+        redirect:'/dashboard',
+        component:DefaultLayout,
+        meta:{requireAuth:true},
+        children:[
+            {path:'/dashboard',name:'Dashboard',component:Dashboard},
+            {path:'/surveys',name:'Surveys',component:Surveys}
+        ]
+    },
+    {path:'/Login',name:'Login',component:Login,meta:{isGuest:true}},
+    {path:'/registration',name:'Registration',component:Registration,meta:{isGuest:true}}
+    
+]
+const router = createRouter({
+    history:createWebHistory(),
+    routes
+})
+
+router.beforeEach((to,form,next)=>{
+
+    if(to.meta.requireAuth && !store.state.users.token){
+        next({name:'Login'});
+    }else if(store.state.users.token && to.meta.isGuest) {
+        next({name:'Dashboard'});
+    }else{
+        next()
+    }
+
+})
+export default router
